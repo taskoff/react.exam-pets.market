5d597b761a26cab2b6eea520477604ab03150b9a
taskoff <marian_taskov@abv.bg>
2020-08-09 16:06:33 +0300

    add comments i details page

--- src/pages/market-pages/details/details.js
+++ src/pages/market-pages/details/details.js
@@ -12,33 +12,70 @@ import getPetInfo from '../../../untils/get-pet-details';
 const Details = ()=> {
 
     const [pet, setPet] = useState({})
+    const [comment, setComment] = useState('')
+    const [comments, setComments] = useState([{comment:'Hi, can I have your phone number?', author: 'Somebody'}])
     
     const context = useContext(MyContext)
     const {id}  = useParams();
 
     const getInfo = async ()=>{
         const res =  await getPetInfo(id);
+        console.log(res)
         setPet(res);
+        // setComments(res.messages)
+    }
+    const renderComments = ()=>{
+        return comments.map(e=>{
+            return (
+                <div>
+                    <p className={styles.comment}>{e.comment}</p>
+                    <p className={styles['comment-author']}><span>Author:</span>{e.author}</p>
+                </div>
+            )
+        })
+    }
+    const commentSubmit = ()=>{
+        console.log(comment)
     }
        
     useEffect( ()=>{
        getInfo()
     },[])
+    // useEffect()
         
         return (
-           
 
             <div className={styles[`${context.color}`]}>
-                    <Header />
-                    <SecondMenu />
+                <Header />
+                <SecondMenu />
+    
+                <Title title="Details"  />
+           
+                <div className={styles['page-container']}>
 
-                    <Title title="Details"  />
-                <div className={styles['notice-container']}>
-                    <Pic path={pet.imageUrl} type='details' class='no-click'/>
-                    <div className={styles['details-box']}>
-                        <p className={styles.text}>{pet.description}</p>
-                        <p className={styles.text}>{pet.price}$</p>
-                        <p className={styles.text}>Autor: {pet.creator}</p>
+                    <div >
+                        <div className={styles['notice-container']}>
+                            <Pic path={pet.imageUrl} type='details' class='no-click'/>
+                            <div className={styles['details-box']}>
+                                <p className={styles.text}>{pet.description}</p>
+                                <p className={styles.text}>Price:<span className={styles.span}>{pet.price}</span>$</p>
+                                <p className={styles.text}>Autor:<span span className={styles.span}>{pet.creator}</span></p>
+                            </div>
+                        </div>
+                        <div className={styles['comments-container']}>
+                            <h3 className={styles['comments-title']}>Comments:</h3>
+                            <div>
+                                <div className={styles['comment-text-box']}>
+                                    <textarea value={comment} onChange={(e)=>{setComment(e.target.value)}} className={styles['comment-text']}></textarea>
+                                </div>
+                                <div>
+                                    <button onClick={commentSubmit} className={styles['comment-button']}>Send</button>
+                                </div>
+                            </div>
+                            <div className={styles['comment-box']}>
+                                {renderComments()}
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
